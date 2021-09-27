7b52567b1862421bcbe3b5e71f85de462488df7b
Hariharan Baskar <hariharanbas@gmail.com>
2021-09-27 17:58:43 +0530

    bugs fixed

--- index.js
+++ index.js
@@ -223,14 +223,17 @@ $(function () {
     if ($(this).val() == "One-Way") {
       document.getElementById("arrival-icon").style.display = "none";
       document.getElementById("arrival-date").style.display = "none";
-      document.getElementById("departure-date").style.width = "286px";
+      document.getElementById("spanarrival").style.display = "none";
+      document.getElementById("departure-date").style.width = "286px"
     } else if ($(this).val() == "Round-Trip") {
       document.getElementById("departure-date").style.width = "101px";
       document.getElementById("arrival-date").style.display = "inline-block";
+      document.getElementById("spanarrival").style.display = "inline-block";
       document.getElementById("arrival-icon").style.display = "inline-block";
     } else {
       document.getElementById("departure-date").style.width = "101px";
       document.getElementById("arrival-date").style.display = "inline-block";
+      document.getElementById("spanarrival").style.display = "inline-block";
       document.getElementById("arrival-icon").style.display = "inline-block";
     }
   });
@@ -242,6 +245,7 @@ $("#onewaybutton").click(function () {
   $("#hiddenbutton").val("One-Way");
   document.getElementById("arrival-icon-mob").style.display = "none";
   document.getElementById("arrival-date-mob").style.display = "none";
+  document.getElementById("spanarrivalmob").style.display = "none";
   document.getElementById("onewaybutton").classList.add("wad-button-clicked");
   document.getElementById("roundtripbutton").classList.remove("wad-button-clicked");
   document.getElementById("multicitybutton").classList.remove("wad-button-clicked");
@@ -251,6 +255,7 @@ $("#roundtripbutton").click(function () {
   $("#hiddenbutton").val("Round-Trip");
   document.getElementById("arrival-date-mob").style.display = "inline-block";
   document.getElementById("arrival-icon-mob").style.display = "inline-block";
+  document.getElementById("spanarrivalmob").style.display = "inline-block";
   document.getElementById("onewaybutton").classList.remove("wad-button-clicked");
   document.getElementById("roundtripbutton").classList.add("wad-button-clicked");
   document.getElementById("multicitybutton").classList.remove("wad-button-clicked");
@@ -260,6 +265,7 @@ $("#multicitybutton").click(function () {
   $("#hiddenbutton").val("Multi-Trip");
   document.getElementById("arrival-date-mob").style.display = "inline-block";
   document.getElementById("arrival-icon-mob").style.display = "inline-block";
+  document.getElementById("spanarrivalmob").style.display = "inline-block";
   document.getElementById("onewaybutton").classList.remove("wad-button-clicked");
   document.getElementById("roundtripbutton").classList.remove("wad-button-clicked");
   document.getElementById("multicitybutton").classList.add("wad-button-clicked");
